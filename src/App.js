1013ed57ae0a5e238e0255fe4c49b3a2f4f67954
Shubham Tandon <iamshubham316@gmail.com>
2023-03-23 11:15:26 +0530

    p2 init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
         <>
           Working react application
+
+          <h1>p2 is here</h1>
         </>
   );
 }
